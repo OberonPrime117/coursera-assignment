ad58140d59f5208f4d10909c0c15ae2e478f742b
unknown <oberon.prime@tutanota.com>
2020-11-11 12:26:32 +0530

    new assgnm

---
+++ course-module4/speakhello.js
@@ -0,0 +1,8 @@
+(function (window) {
+	var helloSpeaker = {};
+	var speakWord = "Hello";
+helloSpeaker.greeting = function (names) {
+  console.log(speakWord + " " + names);
+}
+window.helloSpeaker = helloSpeaker;
+})(window);
